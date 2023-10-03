b24800368ba26b01015edad50b318d7fe1f8005e
NombreRaro <145030957+NombreRaro@users.noreply.github.com>
2023-10-03 22:46:31 +0200

    Add files via upload

---
+++ Entrega3_3.sql
@@ -0,0 +1,36 @@
+SQL_Proyecto 0.0
+
+
+DROP DATABASE IF EXISTS Registro;
+CREATE DATABASE Registro;
+USE Registro;
+
+CREATE TABLE Jugador (
+username VARCHAR NOT NULL,
+password VARCHAR NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE Partida (
+identificador INTEGER PRIMARY KEY NOT NULL,
+año INTEGER PRIMARY KEY NOT NULL,
+mes INTEGER PRIMARY KEY NOT NULL,
+dia INTEGER PRIMARY KEY NOT NULL,
+hora INTEGER PRIMARY KEY NOT NULL,
+minuto INTEGER PRIMARY KEY NOT NULL,
+duración INTEGER PRIMARY KEY NOT NULL,
+ganador TEXT NOT NULL
+)ENGINE = InnoDB;
+--La duracion en minutos
+
+-- La siguiente sección está por terminar, se deja como comentario para trabajar en ella más adelante
+-- CREATE TABLE Participacion (
+-- Jugador TEXT NOT NULL,
+-- Partida INTEGER NOT NULL
+
+INSERT INTO Jugador VALUES ('juanito123', 'macarrones1');
+INSERT INTO Jugador VALUES ('badbunibeibe', 'elreggaetonesunamierda');
+
+INSERT INTO Partida VALUES (1,2023,10,1,18,0,5);
+INSERT INTO Partida VALUES (2,2023,10,1,18,10,6);
+INSERT INTO Partida VALUES (3,2023,10,2,11,30,8);
+ 
